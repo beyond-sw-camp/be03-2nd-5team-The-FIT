772b0a0db6a6429881191834e67502c4b63b785b
kwondohoon1 <kwondohoon1@naver.com>
2024-02-14 19:35:20 +0900

    feat : WorkOut CRUD 생성

---
+++ TheFit/src/main/java/com/example/TheFit/workout/service/WorkOutService.java
@@ -0,0 +1,60 @@
+package com.example.TheFit.workout.service;
+
+
+import com.example.TheFit.workout.domain.WorkOut;
+import com.example.TheFit.workout.dto.WorkOutDto;
+import com.example.TheFit.workout.repository.WorkOutRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class WorkOutService {
+    private final WorkOutRepository workOutRepository;
+
+    @Autowired
+    public WorkOutService(WorkOutRepository workOutRepository) {
+        this.workOutRepository = workOutRepository;
+    }
+    public void create(WorkOutDto workOutDto) {
+        WorkOut workOut = WorkOut.builder()
+                .sets(workOutDto.getSets())
+                .weight(workOutDto.getWeight())
+                .reps(workOutDto.getReps())
+                .restTime(workOutDto.getRestTime())
+                .performance(workOutDto.getPerformance())
+                .build();
+        workOutRepository.save(workOut);
+    }
+
+    public List<WorkOutDto> findAll() {
+        List<WorkOut> workOuts = workOutRepository.findAll();
+        List<WorkOutDto> workOutDtos = new ArrayList<>();
+        for (WorkOut workOut : workOuts) {
+            WorkOutDto workOutDto = WorkOutDto.builder()
+                    .sets(workOut.getSets())
+                    .weight(workOut.getWeight())
+                    .reps(workOut.getReps())
+                    .restTime(workOut.getRestTime())
+                    .performance(workOut.getPerformance())
+                    .build();
+            workOutDtos.add(workOutDto);
+        }
+        return workOutDtos;
+    }
+
+    public WorkOut update(Long id, WorkOutDto workOutDto) {
+        WorkOut workOutUpdate = workOutRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("not found"));
+        workOutUpdate.update(workOutDto);
+        return workOutRepository.save(workOutUpdate);
+    }
+
+    public void delete(Long id) {
+        workOutRepository.deleteById(id);
+    }
+}
+
