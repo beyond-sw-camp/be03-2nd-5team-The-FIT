61860aa48e0167d7cf0304fd48e5b280046205d8
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-15 22:42:18 +0900

    feat : accesstoken을 검증하는 filter 구현 및 config파일에 추가

--- TheFit/src/main/java/com/example/TheFit/security/JwtAuthFilter.java
+++ TheFit/src/main/java/com/example/TheFit/security/JwtAuthFilter.java
@@ -1,5 +1,18 @@
 package com.example.TheFit.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -8,13 +21,40 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+    @Value("${jwt.secretKey}")
+    private String secretKey;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        String bearerToken = ((HttpServletRequest) request).getHeader("Authorization");
+        String refreshToken = ((HttpServletRequest) request).getHeader("refreshToken");
+        if(bearerToken != null){
+            if(!bearerToken.substring(0,7).equals("Bearer ")){
+                throw new AuthenticationServiceException("token의 형식이 맞지않습니다.");
+            }
+            String token = bearerToken.substring(7);
+            Claims claims = null;
+            try {
+                claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_"+claims.get("role")));
+                UserDetails userDetails = new User(claims.getSubject(),"",authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }catch (ExpiredJwtException e){
+//                RefreshToken refreshTokenValue = refreshTokenRepository.findById(decode(token)).orElseThrow(()->new JwtException("refreshToken invalid"));
+//                if(!refreshToken.equals(refreshTokenValue.getRefreshToken())){
+//                    // 모든 접근 거부
+//                    throw new AuthenticationServiceException("refresh token의 형식이 맞지않습니다.");
+//                }
+//                token = jwtTokenProvider.createToken(refreshTokenValue.getEmail(),secretKey);
+            }
+        }
         filterChain.doFilter(request,response);
     }
 }
--- TheFit/src/main/java/com/example/TheFit/security/SecurityConfig.java
+++ TheFit/src/main/java/com/example/TheFit/security/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.TheFit.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    @Autowired
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,6 +32,7 @@ public class SecurityConfig {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
