9660ec061764a56289badf257918b8850df1067d
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-15 21:58:22 +0900

    feat : accessToken과 refreshToken을 만들어 주는 메서드 구현.
    redis에 저장될 key:value를 위해 refreshToken 객체 생성.

--- TheFit/src/main/java/com/example/TheFit/security/TokenService.java
+++ TheFit/src/main/java/com/example/TheFit/security/TokenService.java
@@ -1,26 +1,46 @@
 package com.example.TheFit.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class TokenService {
     @Value("${jwt.secretKey}")
     private String secretKey;
-
     @Value("${jwt.expiration}")
     private Integer expiration;
-
-    public String createAccessToken(Object o){
-
-        return null;
+    @Autowired
+    private final RedisRepository redisRepository;
+    public TokenService(RedisRepository redisRepository) {
+        this.redisRepository = redisRepository;
     }
 
-    public String createRefreshToken(Object o){
+    public String createAccessToken(String email,String role){
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("role",role);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()+expiration*60*1000L))
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .compact();
+    }
 
-        return null;
+    public String createRefreshToken(String email){
+        String refreshTokenValue = UUID.randomUUID().toString();
+        RefreshToken refreshToken = new RefreshToken(email,refreshTokenValue);
+        redisRepository.save(refreshToken);
+        return refreshTokenValue;
     }
 
     public Map<Object,Object> decodeAccessTokenPayload(){
