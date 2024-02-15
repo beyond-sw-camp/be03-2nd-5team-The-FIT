9d2e86741ddc97d4e9bde3ed6a5e5f817c2a2c6b
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-15 21:59:29 +0900

    feat : 로그인 기능 구현
    임시적인 role 부여 추후 수정 필요. 응답 데이터는 토큰 테스팅을 위한 최소한의 데이터를 전송.

--- TheFit/src/main/java/com/example/TheFit/login/LoginController.java
+++ TheFit/src/main/java/com/example/TheFit/login/LoginController.java
@@ -1,25 +1,36 @@
 package com.example.TheFit.login;
 
 import com.example.TheFit.member.domain.Member;
+import com.example.TheFit.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class LoginController {
     @Autowired
     private final LoginService loginService;
-
-    public LoginController(LoginService loginService) {
+    @Autowired
+    private final TokenService tokenService;
+    public LoginController(LoginService loginService, TokenService tokenService) {
         this.loginService = loginService;
+        this.tokenService = tokenService;
     }
 
     @GetMapping("/doLogin")
     public ResponseEntity<TmpResponse> login(LoginRequestDto loginRequestDto){
-        Member object = loginService.login(loginRequestDto);
-
-        return new ResponseEntity<>(new TmpResponse(HttpStatus.OK,"login success",object),HttpStatus.OK);
+        Member member = loginService.login(loginRequestDto);
+        //현재 트레이너와 트레이니의 부모 클래스가 아직 미구현임으로 loginRequestDto에서 임시로 role를 넘겨서 코드진행
+        String accessToken = tokenService.createAccessToken(member.getEmail(),loginRequestDto.getRole());
+        String refreshToken = tokenService.createRefreshToken(member.getEmail());
+        Map<String,Object> memberInfo = new HashMap<>();
+        memberInfo.put("token",accessToken);
+        memberInfo.put("refreshToken",refreshToken);
+        return new ResponseEntity<>(new TmpResponse(HttpStatus.OK,"login success",memberInfo),HttpStatus.OK);
     }
 }
--- TheFit/src/main/java/com/example/TheFit/login/LoginRequestDto.java
+++ TheFit/src/main/java/com/example/TheFit/login/LoginRequestDto.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class LoginRequestDto {
     private String email;
     private String password;
+    private String role;
 }
