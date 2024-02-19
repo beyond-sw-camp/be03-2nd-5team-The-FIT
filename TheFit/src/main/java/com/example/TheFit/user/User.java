fffd5b822c5720601b3ed0ead12efd20a5949ce4
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-19 15:19:27 +0900

    feat : Member, Trainer의 상위 추상 클래스 User 생성

---
+++ TheFit/src/main/java/com/example/TheFit/user/User.java
@@ -0,0 +1,46 @@
+package com.example.TheFit.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Data
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    @Column(nullable = false)
+    public String name;
+    @Column(unique = true, nullable = false)
+    public String email;
+    @Column(nullable = false)
+    public String password;
+    public int cmHeight;
+    public int kgWeight;
+    @Column(unique = true, nullable = false)
+    public String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    public Gender gender;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    public Role role;
+    public String profileImage;
+    @Builder.Default
+    public String delYn="N";
+    @CreationTimestamp
+    public LocalDateTime createdTime;
+    @UpdateTimestamp
+    public LocalDateTime updatedTime;
+}
