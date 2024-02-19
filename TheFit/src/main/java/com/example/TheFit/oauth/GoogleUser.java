12573e16fd26267f7670da7eed06142ca7f60d65
sinaetown <ksw08215@gmail.com>
2024-02-19 16:04:35 +0900

    feat : 기존 로그인에 OAuth 기능 추가 구현

---
+++ TheFit/src/main/java/com/example/TheFit/oauth/GoogleUser.java
@@ -0,0 +1,15 @@
+package com.example.TheFit.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class GoogleUser {
+    public String id;
+    public String email;
+    public Boolean verifiedEmail;
+    public String picture;
+}
