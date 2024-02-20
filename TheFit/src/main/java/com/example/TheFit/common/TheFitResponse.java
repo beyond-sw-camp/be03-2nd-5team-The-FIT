05cdc2885514cefb3d6991064b5bec1bb89bfa52
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-20 09:17:27 +0900

    feat : 현재 프로젝트에 적합한 Custom Response 생성

---
+++ TheFit/src/main/java/com/example/TheFit/common/TheFitResponse.java
@@ -0,0 +1,16 @@
+package com.example.TheFit.common;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TheFitResponse {
+    private HttpStatus status;
+    private String message;
+    private Object result;
+}
