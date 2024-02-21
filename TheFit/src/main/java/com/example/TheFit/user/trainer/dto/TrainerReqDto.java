6fed83a8808f5faadd4b77c14b9cf739a77b9243
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-21 21:24:15 +0900

    feat : Trainer 생성 시 사진도 저장하도록 기능 추가.

--- TheFit/src/main/java/com/example/TheFit/user/trainer/dto/TrainerReqDto.java
+++ TheFit/src/main/java/com/example/TheFit/user/trainer/dto/TrainerReqDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @Builder
@@ -17,6 +18,6 @@ public class TrainerReqDto {
     private int kgWeight;
     private String gender;
     private String role;
-    private String profileImage;
+    private MultipartFile profileImage;
     private String phoneNumber;
 }
